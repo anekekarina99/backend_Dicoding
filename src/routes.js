ee14fd00e286803b6f65a84eda8821ca22569c0d
Ane <42421075+anekekarina99@users.noreply.github.com>
2023-09-24 18:19:11 +0000

    add all

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,33 @@
+const {
+  addBookHandler, getAllBooksHandler, getBookByIdHandler, editBooksByIdHandler,
+  deleteBooksByIdHandler,
+} = require('./handler');
+
 const routes = [
   {
     method: 'POST',
     path: '/books',
-    handler: () => {},
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBooksByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBooksByIdHandler,
   },
 ];
 
